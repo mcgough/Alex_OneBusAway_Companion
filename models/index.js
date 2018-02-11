e81dd8a879f4c883f2570c3a7c9713136c9ad96e
Dan McGough <mcgough.dan@gmail.com>
2018-02-11 12:03:34 -0800

    db moved to models

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/BUS_STOP');
 
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+
 const User = require('./User');
 
 exports.User = User;
