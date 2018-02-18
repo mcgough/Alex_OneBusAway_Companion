b55504e3674a65391c38b9da3946e1d55bcf55bd
Dan McGough <mcgough.dan@gmail.com>
2018-02-17 21:23:09 -0800

    cleanup

--- intents/intents.js
+++ intents/intents.js
@@ -20,7 +20,6 @@ class IntentResponse {
 
 const intents = {
   getArrivals: async (stopId, body) => {
-    console.log(stopId);
     const { Route } = body.request.intent.slots;
     if (Route.value !== '?') {
       try {
@@ -35,20 +34,31 @@ const intents = {
         return err;
       }
     }
-    const noRouteHeard = '<speak>I\'m sorry which route are you asking about?</speak>';
+    const noRouteHeard = `
+      <speak>
+        I\'m sorry which route are you asking about?
+      </speak>`;
     return new IntentResponse(noRouteHeard, {}, false);
   },
   getNext: body => {
     const { attributes } = body.session;
-    const speech = `<speak>After that the next bus will be arriving in ${attributes.parsedData[1].arrival} minutes</speak>`;
+    const speech = `
+      <speak>
+        After that the next bus will be arriving in ${attributes.parsedData[1].arrival} minutes
+      </speak>`;
     return new IntentResponse(speech, attributes.parsedData);
   },
   notUser: body => {
-    const speech = `<speak>What is your bus stop four digit id? It can be found either in the one bus away app or on the sign at the stop.</speak>`;
+    const speech = `
+      <speak>
+        What is your bus stop four digit id?
+        It can be found either in the one bus away app or on the sign at the stop.
+      </speak>`;
     return new IntentResponse(speech, {}, false);
   },
   setUserStop: async (body, deviceId, userId) => {
     const { StopId } = body.request.intent.slots;
+    const parsedStopId = StopId.value.split('').join(' ');
     let speech;
     if (StopId.value !== '?') {
       try {
@@ -59,11 +69,17 @@ const intents = {
           { stop_id: StopId.value },
           { upsert: true },
         );
-        speech = `<speak>Your stop has been set to ${StopId.value}</speak>`;
+        speech = `
+          <speak>
+            Your stop has been set to ${parsedStopId}
+          </speak>`;
         return new IntentResponse(speech, {}, true);
       } catch (err) {
         console.log('error:', err);
-        speech = `<speak>I could not find a bus stop with the id of ${StopId.value.split('').join(' ')}</speak>`
+        speech = `
+          <speak>
+            I could not find a bus stop with the id of ${parsedStopId}.
+          </speak>`
         return new IntentResponse(speech, {}, true);
       }
     }
